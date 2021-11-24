35173f723386c283a03d01e17e6a3945a6b594d8
AyDev <hcordova@blueoceansustainability.com>
2021-11-24 17:06:47 -0600

    FizzBuzz example

--- js/node/fizzBuzz.js
+++ js/node/fizzBuzz.js
@@ -3,14 +3,14 @@ module.exports = function fizzBuzz(size=100) {
 
     for (let n = 1; n <= size; n++) {
         if (n % 3 === 0 || n % 5 === 0) {
-            result += n % 3 === 0 ? 'buzz' : '';
+            result += n % 3 === 0 ? 'fizz' : '';
             result += n % 5 === 0 ? 'buzz' : '';
         } else {
-            result = n;
+            result += n;
         }
 
-        console.log(result);
-        result = '';
+        result += '\n';
     }
 
+    return result;
 }
