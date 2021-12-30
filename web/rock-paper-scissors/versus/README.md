80fd414120f2d2c7d5f22fc940752eb5240a89c6
AyDev <ayaxdev.mx@gmail.com>
2021-12-30 07:03:27 -0600

    Initial commit

---
+++ web/rock-paper-scissors/versus/README.md
@@ -0,0 +1,14 @@
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+
+
+## Info
+
+This is the improve version of the game: Rock, Paper and Scissors, you can play agains the computer.
+
+**This is a work in progress...**
---
+++ web/rock-paper-scissors/versus/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" style="width: 200px; height: 200px;" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+img {
+    widows: 100%;
+    max-width: 100%;
+}
+</style>
